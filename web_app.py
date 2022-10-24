2b7e1702f38458746e88e9d21381eedbdf93234a
hannan-khan-intelops <hkhan@intelops.dev>
2022-10-24 14:44:05 -0500

    updated webapp for pylint

--- web_app.py
+++ web_app.py
@@ -1,15 +1,17 @@
-import app as a
 from flask import Flask
 import pandas as pd
+
+import app as a
+
+"""This is a web app for the docker container example.
+It will calculate fizzbuzz. And display it in a UI."""
 
 app = Flask('docker_example_2')
 
 
 @app.route('/')
 def home():
-    """ Creates a dataframe, and populates the results from
-	fizzbuzz().
-	Returns a nice html string of the dataframe."""
-    df = pd.DataFrame()
-    df['FizzBuzz'] = a.fizzbuzz()
-    return str(df.to_html())
+    """Creates a dataframe, and populates the results from fizzbuzz(). Returns a nice html string of the dataframe."""
+    fizzbuzz_df = pd.DataFrame()
+    fizzbuzz_df['FizzBuzz'] = a.fizzbuzz()
+    return str(fizzbuzz_df.to_html())
