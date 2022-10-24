f71c8e2b291bafe5b5e2180db4160fd5422051c7
hannan-khan-intelops <hkhan@intelops.dev>
2022-10-24 14:46:24 -0500

    updated webapp with module docstring

--- web_app.py
+++ web_app.py
@@ -1,17 +1,18 @@
+"""This is a web app for the docker container example.
+It will calculate fizzbuzz. And display it in a UI."""
+
 from flask import Flask
 import pandas as pd
 
 import app as a
-
-"""This is a web app for the docker container example.
-It will calculate fizzbuzz. And display it in a UI."""
 
 app = Flask('docker_example_2')
 
 
 @app.route('/')
 def home():
-    """Creates a dataframe, and populates the results from fizzbuzz(). Returns a nice html string of the dataframe."""
+    """Creates a dataframe, and populates the results from fizzbuzz().
+    Returns a nice html string of the dataframe."""
     fizzbuzz_df = pd.DataFrame()
     fizzbuzz_df['FizzBuzz'] = a.fizzbuzz()
     return str(fizzbuzz_df.to_html())
